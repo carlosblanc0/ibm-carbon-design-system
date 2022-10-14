99bf4fed425119b77bce39b9218108cd157ce801
Carlos A. Blanco <carlosbusinessinquiries@gmail.com>
2022-10-14 12:16:57 -0400

    Pre-configure styles + remove starter boilerplates

---
+++ carbon/src/components/Text.tsx
@@ -0,0 +1,5 @@
+export function Text() {
+    return (
+        <span className="text-cyan-500">Social@IBM</span>
+    )
+}
--- carbon/tailwind.config.cjs
+++ carbon/tailwind.config.cjs
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html',
+    './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      'xs': '0.875rem',
+      'sm': '1rem',
+      'md': '1.125rem',
+      'lg': '1.25rem',
+      'xl': '1.5rem',
+      '2xl': '2rem',
+    },
+    colors: {
+      'black': '#161616',
+      'gray-900': '#202024',
+      'gray-800': '#262626',
+      'gray-400': '#7C7C8A',
+      'gray-100': '#C4C4CC',
+      'gray-200': '#F1F1F1',
+
+      'cyan-500': '#0F62FE',
+      'cyan-300': '#78A9FF',
+
+    },
     extend: {
       fontFamily: {
         sans: 'IBM Plex Sans, sans-serif'
